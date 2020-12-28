bc75c7aa505a87d104437c6e8175615c1518fae3
Mark <mark@remarkablemark.org>
2020-12-28 16:42:33 -0500

    feat(index): add and export `tranpose` function
    Inspired by https://repl.it/@remarkablemark/Swap-array-row-and-column

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+type Matrix = any[][];
+
+interface Options {
+  excludeEmpty?: boolean;
+}
+
+/**
+ * Tranposes a matrix by switching the row and column indices of an associative array.
+ */
+export function transpose(matrix: Matrix, options: Options = {}): Matrix {
+  const rowsCount = matrix.length;
+  const output: Matrix = [];
+
+  for (let r = 0; r < rowsCount; r++) {
+    const row = matrix[r];
+    const columnsCount = row.length;
+
+    for (let c = 0; c < columnsCount; c++) {
+      const item = row[c];
+      output[c] = output[c] || [];
+      if (options.excludeEmpty) {
+        output[c].push(item);
+      } else {
+        output[c][r] = item;
+      }
+    }
+  }
+
+  return output;
+}
